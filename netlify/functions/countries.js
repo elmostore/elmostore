e0b01bb5c81a5c61eb10de8d947f33eaeaee4d4d
Elmouafek ELKALI <albirtdomi@gmail.com>
2025-12-01 23:34:50 +0100

    Change country data source from RestCountries to Printful

--- netlify/functions/countries.js
+++ netlify/functions/countries.js
@@ -1,10 +1,10 @@
 export default async (req, res) => {
   try {
-    const result = await fetch("https://restcountries.com/v3.1/all");
-    const json = await result.json();
+    const response = await fetch("https://api.printful.com/countries");
+    const data = await response.json();
 
-    res.status(200).json(json);
-  } catch (err) {
+    res.status(200).json(data.result);
+  } catch (error) {
     res.status(500).json({ error: "Failed to load countries" });
   }
 };
