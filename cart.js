9a9db32ecebf83fd46970de3af70f5c1d0b54588
Elmouafek ELKALI <albirtdomi@gmail.com>
2025-12-01 10:21:01 +0100

    Delete cart.js

--- cart.js
+++
@@ -1,6 +0,0 @@
-export function addToCart(id) {
-  const cart = JSON.parse(localStorage.getItem("cart") || "[]")
-  cart.push({ product_id: id, qty: 1 })
-  localStorage.setItem("cart", JSON.stringify(cart))
-  alert("تمت الإضافة")
-}
