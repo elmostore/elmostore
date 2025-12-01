6d59a7643be78d48f5656ecd5bd6a75633fa3d2a
Elmouafek ELKALI <albirtdomi@gmail.com>
2025-12-01 23:47:43 +0100

    Update countries.js

--- netlify/functions/countries.js
+++ netlify/functions/countries.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   try {
-    const response = await fetch("https://restcountries.com/v3.1/all");
+    const response = await fetch("https://api.printful.com/countries");
     const data = await response.json();
 
     res.status(200).json(data.result);
