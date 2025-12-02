a7215229418f05e207893a791c339a46f6a87abe
Elmouafek ELKALI <albirtdomi@gmail.com>
2025-12-02 16:38:57 +0100

    Update countries.js

--- netlify/functions/countries.js
+++ netlify/functions/countries.js
@@ -1,18 +1,16 @@
 export async function handler() {
+  //1
   const res = await fetch("https://api.printful.com/countries", {
-    headers: {
-      Authorization: "Bearer " + process.env.PRINTFUL_TOKEN
-    }
-  });
-
-  const data = await res.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data.result)
-  };
-}
-
+    headers: {Authorization: "Bearer " + process.env.PRINTFUL_TOKEN}
+                                                                }    );
+  //2
+ const data = await res.json();
+  //3
+  return { statusCode: 200,
+                 body: JSON.stringify(data.result)
+        };
+                                 }
+//========================================================================================
 /*export default async (req, res) => {
   try {
     const response = await fetch("https://api.printful.com/countries");
